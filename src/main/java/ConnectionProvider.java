2c5fe65abb8f5be06465ab7abbfe8030a09c3e06
Himanshu5899 <112534440+Himanshu5899@users.noreply.github.com>
2023-06-20 02:11:34 +0530

    Add files via upload

---
+++ src/main/java/ConnectionProvider.java
@@ -0,0 +1,31 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class ConnectionProvider {
+     public static Connection getCon() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/blooddonation" ,"root","");
+            return con;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Connection getCon(String jdbcmysqllocalhost3307bbmszeroDateTimeBeh, String root) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    
+}
---
+++ src/main/java/com/mycompany/blooddoation/BloodDoation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.blooddoation;
+
+/**
+ *
+ * @author hp
+ */
+public class BloodDoation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
